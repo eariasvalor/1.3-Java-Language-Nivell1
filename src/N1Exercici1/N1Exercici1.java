23edae3c9f23170e1198ef9559cb449d43374fed
Esther Arias <eariasvalor@gmail.com>
2025-09-25 13:48:18 +0200

    feat: charged ArrayList and iterator to print elements

--- src/N1Exercici1/N1Exercici1.java
+++ src/N1Exercici1/N1Exercici1.java
@@ -4,7 +4,24 @@ import java.util.ArrayList;
 
 public class N1Exercici1 {
     public static void main (String[] args){
-        ArrayList<String> months = new ArrayList<String>();
+        ArrayList<Month> months = new ArrayList<Month>();
+
+        months.add(new Month("Gener"));
+        months.add(new Month ("Febrer"));
+        months.add(new Month ("Març"));
+        months.add(new Month ("Abril"));
+        months.add(new Month ("Maig"));
+        months.add(new Month ("Juny"));
+        months.add(new Month ("Juliol"));
+        months.add(new Month ("Setembre"));
+        months.add(new Month ("Octubre"));
+        months.add(new Month ("Novembre"));
+        months.add(new Month ("Desembre"));
+
+        for (Month m : months) {
+            System.out.println(m.getName());
+        }
+
 
     }
 }
