938424b8c5e5b7430f8631955b95063136cf10ed
Esther Arias <eariasvalor@gmail.com>
2025-09-25 16:51:13 +0200

    feat: added HashSet

--- src/N1Exercici1/N1Exercici1.java
+++ src/N1Exercici1/N1Exercici1.java
@@ -1,6 +1,7 @@
 package N1Exercici1;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class N1Exercici1 {
     public static void main (String[] args){
@@ -22,12 +23,41 @@ public class N1Exercici1 {
             System.out.println(m.getName());
         }
 
-        System.out.println();
+        System.out.println("");
 
         months.add(7, new Month("Agost"));
 
         for (Month m : months) {
             System.out.println(m.getName());
         }
+
+        HashSet<Month> months2 = new HashSet<Month>();
+
+        months2.add(new Month("Gener"));
+        months2.add(new Month ("Febrer"));
+        months2.add(new Month ("Març"));
+        months2.add(new Month ("Abril"));
+        months2.add(new Month ("Maig"));
+        months2.add(new Month ("Juny"));
+        months2.add(new Month ("Juliol"));
+        months2.add(new Month ("Setembre"));
+        months2.add(new Month ("Octubre"));
+        months2.add(new Month ("Novembre"));
+        months2.add(new Month ("Desembre"));
+
+        //HashSet doesn't allow 2 arguments because it is a non-ordered set.
+     //   months2.add(7, new Month ("Agost"));
+
+        months2.add(new Month ("Agost"));
+        months2.add(new Month ("Agost"));
+
+        System.out.println("HashSet:");
+
+        for (Month m : months2) {
+            System.out.println(m.getName());
+        }
+
     }
+
+
 }
