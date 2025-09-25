e64c67f019daed4b651e09cf14e8ca14788b5429
Esther Arias <eariasvalor@gmail.com>
2025-09-25 13:35:35 +0200

    feat: add ArrayList

--- src/N1Exercici1/N1Exercici1.java
+++ src/N1Exercici1/N1Exercici1.java
@@ -1,8 +1,10 @@
 package N1Exercici1;
 
+import java.util.ArrayList;
+
 public class N1Exercici1 {
     public static void main (String[] args){
-
+        ArrayList<String> months = new ArrayList<String>();
 
     }
 }
