eadd7dcc611a8ab25a8cfea3163b523bd77ed15c
Esther Arias <eariasvalor@gmail.com>
2025-09-30 10:58:51 +0200

    feat: add construtor, getters and setters

--- src/n1exercici3/User.java
+++ src/n1exercici3/User.java
@@ -1,5 +1,27 @@
 package n1exercici3;
 
 public class User {
+    private  String name;
+    private int score;
 
+    public User(String name){
+        this.name = name;
+        this.score = 0;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getScore(){
+        return this.score;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void addPoint(){
+        this.score++;
+    }
 }
