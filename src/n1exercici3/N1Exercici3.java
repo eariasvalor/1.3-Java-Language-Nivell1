dd4221d34b13c0c06d918a4f0b20aedebbd0e362
Esther Arias <eariasvalor@gmail.com>
2025-09-30 12:34:02 +0200

    feat: created for with questions and answers

--- src/n1exercici3/N1Exercici3.java
+++ src/n1exercici3/N1Exercici3.java
@@ -1,18 +1,38 @@
 package n1exercici3;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class N1Exercici3 {
     public static void main (String[] args){
         HashMap<String, String> countries = new HashMap<>();
         ReadFile reader = new ReadFile();
+        Scanner scanner = new Scanner(System.in);
+        boolean result = false;
+        String country = "";
+        String answer = "";
+
         reader.feedCountries("src/resources/countries.txt");
 
 
+        System.out.println("Hello! What's your name?");
+        User user1 = new User(scanner.nextLine());
 
+
+        for (int i  = 0; i < 10; i++){
+            country = reader.showRandomCountry();
+            System.out.println("What's the capital of " + country + "?");
+            answer = scanner.nextLine();
+            result = reader.checkAnswer(country, answer);
+            user1.calculateScore(result);
+            if(result) {
+                System.out.println("Your answer is right. You got 1 point!");
+            } else {
+                System.out.println("Your answer is wrong. The right answer was: " + reader.getCapital(country));
+            }
+
+            System.out.println("Your score is: " + user1.getScore());
+        }
     }
 }
