13dd3e3c8857bc0911864844613dfef291240160
Esther Arias <eariasvalor@gmail.com>
2025-10-01 10:37:29 +0200

    feat: improve list feeding with a for

--- src/n1exercici2/N1Exercici2.java
+++ src/n1exercici2/N1Exercici2.java
@@ -9,21 +9,15 @@ public class N1Exercici2 {
         List<Integer> list1 = new ArrayList<>();
         List<Integer> list2 = new ArrayList<>();
 
-        list1.add(1);
-        list1.add(2);
-        list1.add(3);
-        list1.add(4);
-        list1.add(5);
-        list1.add(6);
-        list1.add(7);
-        list1.add(8);
-        list1.add(9);
-        list1.add(10);
+        for (int i = 1; i < 11; i++) {
+            list1.add(i);
+        }
 
         ListIterator<Integer> it = list1.listIterator(list1.size());
         while(it.hasPrevious()) {
             list2.add(it.previous());
         }
+
 
         System.out.println("Ascendant order: " + list1);
         System.out.println("Descendant order: " + list2);
